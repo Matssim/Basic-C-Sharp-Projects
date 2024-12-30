2f460f936d46818e951b89f749b145baf4b56ccc
Mats Simonsen <mats.simonsen@outlook.com>
2024-12-30 15:25:27 -0800

    Acme Accounting Systems - Welcome Screen

---
+++ Misc. Assignments and Challenges/Step152_Assignment/Step152_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step152_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, We\'re \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
