e3c3125e7c6d499ebc3fbdf2c794f9d58d61c5ef
Mats Simonsen <mats.simonsen@outlook.com>
2025-01-03 19:03:06 -0800

    Uploading Step 246 Assignment

---
+++ Misc. Assignments and Challenges/Step246_Assignment/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctions
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your preferred prefix?");
+            string prefix = Console.ReadLine();
+            prefix = prefix.ToUpper(); //Converts string to all uppercase letters
+            Console.WriteLine("Please enter your email address:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + prefix + ". " + name + "\nWe'll reach out to you on " + email); //concatenates strings
+
+            StringBuilder contact = new StringBuilder(); //adds each listed element to the "contact" string variable
+            contact.Append("We have registered:\n");
+            contact.Append(email);
+            contact.Append("\nas your primary email address");
+
+            Console.WriteLine(contact);
+            Console.ReadLine();
+
+        }
+    }
+}
