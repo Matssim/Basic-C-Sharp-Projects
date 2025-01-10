747a57395aeaff2d8955754ee6381e6c19586fd3
Mats Simonsen <mats.simonsen@outlook.com>
2025-01-10 15:15:21 -0800

    Uploading step 292 assignment

---
+++ Misc. Assignments and Challenges/Step292_Assignment/MainMethodApp/MainMethodApp/MathOperations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MainMethodApp
+{
+    //Establishes "MathOperations" as a public class, which makes it accessible accross the namespace
+    public class MathOperations
+    {
+        //Establishes the "SubtractTwo()" method as: public, to make to as it accessible accross the namespace; static, as  
+        // it is not an object of MathOperations, but a method; and with an integer return type. The UserInt variable is 
+        // passed in
+        public static int SubtractTwo(int UserInt)
+        {
+            //Subtracts 2 from UserInt and establishes the result as an integer variable that is returned from the method
+            int subtractionResult = UserInt - 2;
+            return subtractionResult;
+        }
+
+        //Establishes the "IncreaseNumber()" method as: public, to make to as it accessible accross the namespace; static,  
+        // as it is not an object of MathOperations, but a method; and with an decimal return type. The UserDec variable is
+        // passed in
+        public static decimal IncreaseNumber(decimal UserDec)
+        {
+            //Multiplies UserDec by 1.2 and establishes the result as an decimal variable that is returned from the method
+            decimal multiplicationResult = UserDec * 1.2m;
+            return multiplicationResult;
+        }
+
+        //Establishes a second version of the "IncreaseNumber()" method, but instead of the UserDec variable it passes in 
+        // the multiplicationResult variable from the method above and the UserMult variable
+        public static int IncreaseNumber(decimal multiplicationResult, string UserMult)
+        {
+            //Converts the Usermult string and the multiplicationResult decimal to integers, multipies them by each other
+            // and returns the result as the integer variable increaseMulitplied
+            int increaseMulitplied = Convert.ToInt32(multiplicationResult) * Convert.ToInt32(UserMult);
+            return increaseMulitplied;
+        }
+    }
+}
---
+++ Misc. Assignments and Challenges/Step292_Assignment/MainMethodApp/MainMethodApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MainMethodApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Establishes the intended code to run
+            try
+            { 
+
+                //Prints a promt for the user to enter a number
+                Console.WriteLine("Please enter a number to subtract two from it");
+                //Reads the user input and establishes it as the integer variable "UserInt"
+                int UserInt = Convert.ToInt32(Console.ReadLine());
+                //Instantiates the MathOperations class as subtractionResult, calling in the SubtractTwo method, passing
+                // in the UserInt interger and prints the result
+                int subtractionResult = MathOperations.SubtractTwo(UserInt);
+                Console.WriteLine(UserInt + " - 2 = " + subtractionResult);
+
+                //Prints a promt for the user to enter a number
+                Console.WriteLine("Please enter a decimal number to increase it by 20%");
+                //Reads the user input and establishes it as the decimal variable "UserDec"
+                decimal UserDec = Convert.ToDecimal(Console.ReadLine());
+                //Instantiates the MathOperations class as increasedDec, calling in the IncreaseNumber method, passing
+                // in the UserDec decimal and prints the result
+                decimal increasedDec = MathOperations.IncreaseNumber(UserDec);
+                Console.WriteLine("if you increase " + UserDec + " by 20%, you get " + increasedDec);
+
+                //Prints a promt for the user to enter a number
+                Console.WriteLine("Please enter a whole number to multiply the increased number by");
+                //Reads the user input and establishes it as the string variable "UserMult"
+                string UserMult = Console.ReadLine();
+                //Instantiates the MathOperations class as finalResult, calling in the IncreaseNumber method, passing
+                // in the UserDec decimal and the UserMult string. Then, the result is printed
+                int finalResult = MathOperations.IncreaseNumber(UserDec, UserMult);
+                Console.WriteLine(increasedDec + " * " + UserMult + " = " + finalResult);
+            }
+
+            //Cathes any execption and assign is to the variable "ex"
+            catch (Exception ex)
+            {
+                //Prints the message corresponding to the error and a custom error message
+                Console.WriteLine(ex.Message + "\nPlease enter values as instructed");
+            }
+
+            //Establishes an end statement that will print regardless of the error handling 
+            finally
+            {
+                Console.WriteLine("This is the end of the program");
+                Console.ReadLine();
+            }
+        }
+    }
+}
