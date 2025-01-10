25906657b3e133008535fb450d375f854f7099a0
Mats Simonsen <mats.simonsen@outlook.com>
2025-01-09 19:40:21 -0800

    Uploading step 288 assignment

---
+++ Misc. Assignments and Challenges/Step288_Assignment/CallingMethodsApp/CallingMethodsApp/MathOperations.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CallingMethodsApp
+{
+    //Establishes "MathOperations" as a public class, which makes it accessible accross the namespace
+    public class MathOperations
+    {
+        //Establishes the "AddToFive()" method as: public, to make to as it accessible accross the namespace; static, as it is  
+        // not an object of MathOperations, but a method; and with an integer return type. The Userint variable is passed in
+        public static int AddToFive(int Userint)
+        {
+            //Adds Userint to 5 and establishes the result as an integer variable that is returned from the method
+            int additionResult = Userint + 5;
+            return additionResult;
+        }
+        //Establishes the "MultiplyByFive()" method as: public, to make to as it accessible accross the namespace; static, as it is  
+        // not an object of MathOperations, but a method; and with an integer return type. The Userint variable is passed in
+        public static int MultiplyByFive(int Userint)
+        {
+            //Multiplies Userint by 5 and establishes the result as an integer variable that is returned from the method
+            int multiplicationResult = Userint * 5;
+            return multiplicationResult;
+        }
+        //Establishes the "DivideByTwo()" method as: public, to make to as it accessible accross the namespace; static, as it is  
+        // not an object of MathOperations, but a method; and with an integer return type. The Userint variable is passed in
+        public static int DivideByTwo(int Userint)
+        {
+            //Divides Userint by 2 and establishes the result as an integer variable that is returned from the method
+            int divisionResult = Userint / 2;
+            return divisionResult;
+        }
+    }
+}
---
+++ Misc. Assignments and Challenges/Step288_Assignment/CallingMethodsApp/CallingMethodsApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CallingMethodsApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Prints a promt for the user to enter a number
+            Console.WriteLine("Please enter a number to perform math operations on");
+            //Reads the user input and establishes it as the integer variable "Userint"
+            int Userint = Convert.ToInt32(Console.ReadLine());
+            //Passes the Userint variable to the "AddToFive()" method in the "MathOperations" class and establishes the output
+            // as the "additionResult" variable and prints it to a string outlining the math problem 
+            int additionResult = MathOperations.AddToFive(Userint);
+            Console.WriteLine(Userint + " + 5 = " + additionResult);
+            //Passes the Userint variable to the "MultiplyByFive()" method in the "MathOperations" class and establishes the output
+            // as the "multiplicationResult" variable and prints it to a string outlining the math problem 
+            int multiplicationResult = MathOperations.MultiplyByFive(Userint);
+            Console.WriteLine(Userint + " * 5 = " + multiplicationResult);
+            //Passes the Userint variable to the "DivideByTwo()" method in the "MathOperations" class and establishes the output
+            // as the "divisionResult" variable and prints it to a string outlining the math problem 
+            int divisionResult = MathOperations.DivideByTwo(Userint);
+            Console.WriteLine(Userint + " / 2 = " + divisionResult);
+            Console.ReadLine();
+        }
+    }
+}
