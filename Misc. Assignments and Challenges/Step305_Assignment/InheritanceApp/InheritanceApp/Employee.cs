f80693061adedb7dea6848d7266242b45d62afcb
Mats Simonsen <mats.simonsen@outlook.com>
2025-01-16 14:42:10 -0800

    Uploading step 298 assignment

---
+++ Misc. Assignments and Challenges/Step305_Assignment/InheritanceApp/InheritanceApp/Employee.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InheritanceApp
+{
+    //Creates the public class Employee, that inherits the properties of the Person class
+    public class Employee: Person
+    {
+        //Creates an integer variable Id that can be assisgned values when an object of the class 
+        // is instantiated
+        public int Id { get; set; }
+    }
+}
---
+++ Misc. Assignments and Challenges/Step305_Assignment/InheritanceApp/InheritanceApp/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InheritanceApp
+{
+    //Creates the public Person class
+    public class Person
+    {
+        //Creates two string variables, FirstName and LastName, that can be assisgned values when an object of the class 
+        // is instantiated
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //Creates a method that does not return anything, but prints the instantiated values to a string
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
