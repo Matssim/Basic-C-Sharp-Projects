d3f818286e02cb59fee09d5fbdd81e0b71c2e2ac
Mats Simonsen <mats.simonsen@outlook.com>
2025-01-17 20:32:58 -0800

    Uploading step 327 assignment

---
+++ Misc. Assignments and Challenges/Step327_Assignment/OverloadOperatorApp/OverloadOperatorApp/Employee.cs
@@ -0,0 +1,38 @@
+﻿
+namespace OverloadOperatorApp
+{
+    //Creates the public class Employee
+    public class Employee
+    {
+        //Establishes three attributes for objects of the class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overloads the == and != operators so that it only compares the Id attributes of the objects that are passed 
+        // in and returns a boolean value denoting whether the Id's match 
+        public static bool operator==(Employee sampleEmployee1, Employee sampleEmployee2)
+        {
+            if (sampleEmployee1.Id == sampleEmployee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator!=(Employee sampleEmployee1, Employee sampleEmployee2)
+        {
+            if (sampleEmployee1.Id != sampleEmployee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Misc. Assignments and Challenges/Step327_Assignment/OverloadOperatorApp/OverloadOperatorApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OverloadOperatorApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Instantiates two objects of the Employee class with different values assigned to the attributes
+            Employee sampleEmployee1 = new Employee() { Id = 1, FirstName = "Sample1", LastName = "Employee1" };
+            Employee sampleEmployee2 = new Employee() { Id = 2, FirstName = "Sample2", LastName = "Employee2" };
+            //Creates an if/else statement that deploys the overloaded == opertaor to compare only the Id property 
+            // of the two Employee objects and prints the values to a string commensurate to that comparison
+            if (sampleEmployee1 == sampleEmployee2)
+            {
+                Console.WriteLine(sampleEmployee1.FirstName + " " + sampleEmployee1.LastName + " and " + 
+                    sampleEmployee2.FirstName + " " + sampleEmployee2.LastName + " has the same Id.");
+            }
+            else
+            {
+                Console.WriteLine(sampleEmployee1.FirstName + " " + sampleEmployee1.LastName + " and " +
+                    sampleEmployee2.FirstName + " " + sampleEmployee2.LastName + " does not have the same Id.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
